b1cbb783eaaf4a99ef97712a477acaf064de6ec8
DenisKut <tarelko_den@list.ru>
2024-12-17 08:11:41 +0300

    Added TODO list
      Minimal fixes with TODO list

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Home from './pages/home/Home.jsx'
-import PWABadge from './PWABadge.jsx'
+import PWABadge from './components/ui/pwa-badge/PWABadge.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
